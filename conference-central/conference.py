939882bf23050c05f410421b661cfc6e041e0913
Brian Chen <bchen.chen93@gmail.com>
2016-01-25 10:46:38 +1300

    feat: Starting part 5

--- conference-central/conference.py
+++ conference-central/conference.py
@@ -35,8 +35,9 @@ from models import ConferenceForms
 from models import ConferenceQueryForm
 from models import ConferenceQueryForms
 from models import TeeShirtSize
+from models import StringMessage
 
-from additions.utils import getUserId
+from utils import getUserId
 
 from settings import WEB_CLIENT_ID
 
@@ -83,7 +84,7 @@ CONF_POST_REQUEST = endpoints.ResourceContainer(
 
 
 @endpoints.api(name='conference', version='v1',
-    allowed_client_ids=['676964429571-37770qoofor3j4kf57ab6lvjb432vavb.apps.googleusercontent.com', API_EXPLORER_CLIENT_ID],
+    allowed_client_ids=[WEB_CLIENT_ID, API_EXPLORER_CLIENT_ID],
     scopes=[EMAIL_SCOPE])
 class ConferenceApi(remote.Service):
     """Conference API v0.1"""
@@ -490,6 +491,41 @@ class ConferenceApi(remote.Service):
 
 # - - - Announcements - - - - - - - - - - - - - - - - - - - -
 
-# TODO 1
+    @staticmethod
+    def _cacheAnnouncement():
+        """Create Announcement & assign to memcache; used by
+        memcache cron job & putAnnouncement().
+        """
+        confs = Conference.query(ndb.AND(
+            Conference.seatsAvailable <= 5,
+            Conference.seatsAvailable > 0)
+        ).fetch(projection=[Conference.name])
+
+        if confs:
+            # If there are almost sold out conferences,
+            # format announcement and set it in memcache
+            announcement = '%s %s' % (
+                'Last chance to attend! The following conferences '
+                'are nearly sold out:',
+                ', '.join(conf.name for conf in confs))
+            memcache.set(MEMCACHE_ANNOUNCEMENTS_KEY, announcement)
+        else:
+            # If there are no sold out conferences,
+            # delete the memcache announcements entry
+            announcement = ""
+            memcache.delete(MEMCACHE_ANNOUNCEMENTS_KEY)
+
+        return announcement
+
+
+    @endpoints.method(message_types.VoidMessage, StringMessage,
+            path='conference/announcement/get',
+            http_method='GET', name='getAnnouncement')
+    def getAnnouncement(self, request):
+        """Return Announcement from memcache."""
+        # TODO 1
+        # return an existing announcement from Memcache or an empty string.
+        announcement = ""
+        return StringMessage(data=announcement)
 
 api = endpoints.api_server([ConferenceApi]) # register API
